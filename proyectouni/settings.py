fd37952125d739adfa0d3ff4e74553b034ed37c3
Erick Rojas <151458346+Shadowkirito123@users.noreply.github.com>
2024-07-23 19:17:56 -0400

    colocando la fecha de inicio y final en español

--- proyectouni/settings.py
+++ proyectouni/settings.py
@@ -113,11 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -131,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.conf.locale.es import formats as es_formats
+
+es_formats.DATETIME_FORMAT = "j F Y, H:i"
